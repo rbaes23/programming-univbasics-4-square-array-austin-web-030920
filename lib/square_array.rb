a06bca854280f056a54b87a2610d177ef97cc287
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-20 19:18:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-    options_from_collection_for_select new_array.push(array[counter] ** 2)
+    print new_array.push(array[counter] ** 2)
     counter+=1
   end
 end
